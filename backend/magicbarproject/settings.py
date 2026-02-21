ab61f29cb169bd771d839b14dbfffd62a8855a1f
Dustin Swayze <dustin@DESKTOP-LQBSSKV.localdomain>
2026-02-20 23:13:46 -0500

    adding login page from local

--- backend/magicbarproject/settings.py
+++ backend/magicbarproject/settings.py
@@ -169,6 +169,11 @@ CLOSING_REPORT_RECIPIENTS = [
 ]
 
 
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+
 
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
--- backend/magicbarproject/urls.py
+++ backend/magicbarproject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path("", include("reports.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
